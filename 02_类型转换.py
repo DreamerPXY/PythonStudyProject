f6c114df6922dfdf558d44baa0aead6a56b94a35
pengxy <pengxy@zenmen.com>
2021-11-18 11:53:22 +0800

    结构目录调整

--- 02_类型转换.py
+++
@@ -1,7 +0,0 @@
-name = "123"
-nameForInt = int(name)
-print(nameForInt)
-
-intName = 123
-nameForStr = str(intName)
-print(nameForStr)
--- 05_function_变量作用域问题.py
+++
@@ -1,5 +0,0 @@
-global a
-a = 1
-def name1():
-    a+=a
-    return a
